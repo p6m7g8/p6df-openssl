6488642f1d86eff7544f9763a41812d382f32073
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-05 14:10:26 -0400

    feat(yum): adds yum support
    also chases p6m7g8/p6openssl -> p6m7g8/p6common/lib/openssl

--- init.zsh
+++ init.zsh
@@ -8,9 +8,22 @@
 ######################################################################
 p6df::modules::openssl::deps() {
 	ModuleDeps=(
-		p6m7g8/p6openssl
 		Matty9191/ssl-cert-check
 	)
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::openssl::external::yum()
+#
+#  Environment:	 XXX
+#>
+######################################################################
+p6df::modules::openssl::external::yum() {
+
+	# XXX: 1.0.x
+	sudo yum install openssl
 }
 
 ######################################################################
@@ -30,6 +43,8 @@ p6df::modules::openssl::external::brew() {
 #
 # Function: p6df::modules::openssl::init()
 #
+#  Depends:	 p6_path
+#  Environment:	 P6_DFZ_SRC_DIR SSLKEYLOGFILE
 #>
 ######################################################################
 p6df::modules::openssl::init() {
